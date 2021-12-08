2f78735c0b6a80a58a9229af27dadecfc3f4b65c
kubit <contero@gmail.com>
2021-12-07 22:59:02 -0500

    scanner work

--- src/jLox/Scanner.java
+++ src/jLox/Scanner.java
@@ -3,7 +3,9 @@ package jLox;
 import static jLox.TokenType.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Scanner 
 {
@@ -12,12 +14,36 @@ public class Scanner
 	private int start = 0,
 			current = 0,
 			line = 1;
-	
+	private static final Map<String, TokenType> keywords;
+
+	static
+	{
+		keywords = new HashMap<>();
+		keywords.put("and", AND);
+		keywords.put("class", CLASS);
+		keywords.put("else", ELSE);
+		keywords.put("false", FALSE);
+		keywords.put("for", FOR);
+		keywords.put("fun", FUN);
+		keywords.put("if", IF);
+		keywords.put("nil", NIL);
+		keywords.put("or", OR);
+		keywords.put("print", PRINT);
+		keywords.put("return", RETURN);
+		keywords.put("super", SUPER);
+		keywords.put("this", THIS);
+		keywords.put("true", TRUE);
+		keywords.put("var", VAR);
+		keywords.put("while", WHILE);
+	}
+
 	Scanner(String source)
 	{
 		this.source = source;
 	}
-	
+
+
+
 	List<Token> scanTokens()
 	{
 		while (!isAtEnd())
@@ -57,7 +83,10 @@ public class Scanner
 				addToken(COMMA);
 				break;
 			case '.':
-				addToken(DOT);
+				if (isDigit(peek()))
+					number();
+				else
+					addToken(DOT);
 				break;
 			case '-':
 				addToken(MINUS);
@@ -71,6 +100,55 @@ public class Scanner
 			case '*':
 				addToken(STAR);
 				break;
+			case '!':
+				addToken(match('!') ? BANG_EQUAL : BANG);
+				break;
+			case '=':
+				addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+				break;
+			case '<':
+				addToken(match('=') ? LESS_EQUAL : LESS);
+				break;
+			case '>':
+				addToken(match('=') ? GREATER_EQUAL : GREATER);
+				break;
+			case '/':
+				if (match('/'))
+				{
+					while(peek() != '\n' && !isAtEnd()) advance();
+				}
+				else if (match('*'))
+				{
+					blockComment();
+				}
+				else
+				{
+					addToken(SLASH);
+				}
+				break;
+			case ' ':
+			case '\r':
+			case '\t':
+				break;
+			case '\n':
+				line++;
+				break;
+			case '"':
+				string();
+				break;
+			default:
+				if (isDigit(c))
+				{
+					number();
+				}
+				else if (isAlpha(c))
+				{
+					identifier();
+				}
+				else
+				{
+					Lox.error(line, "Unexpected Character n'shit");
+				}
 		}
 	}
 	
@@ -88,5 +166,142 @@ public class Scanner
 	{
 		String text = source.substring(start, current);
 		tokens.add(new Token(type, text, literal, line));
+	}
+
+	private boolean match(char expected)
+	{
+		if (isAtEnd()) return false;
+		if (source.charAt(current) != expected) return false;
+
+		current++;
+		return true;
+	}
+
+	private char peek()
+	{
+		if (isAtEnd()) return '\0';
+		return source.charAt(current);
+	}
+
+	private void string()
+	{
+		while (peek() != '"' && ! isAtEnd()) {
+			if (peek() == '\n') {
+				line++;
+			}
+
+			advance();
+		}
+
+		if (isAtEnd())
+		{
+			Lox.error(line, "Unterminated string n'shit");
+			return;
+		}
+
+		advance();
+
+		String value = source.substring(start + 1, current -1);
+		addToken(STRING, value);
+	}
+
+	private boolean isDigit(char c)
+	{
+		return c >= '0' && c <= '9';
+	}
+
+	private boolean isAlpha(char c)
+	{
+		return (c >= 'a' && c <= 'z') ||
+				( c >= 'A' && c <= 'Z') ||
+				( c == '_');
+	}
+
+	private boolean isAlphaNumeric(char c)
+	{
+		return isAlpha(c) || isDigit(c);
+	}
+
+	private void number()
+	{
+		if (source.charAt(start) != '.')
+		{
+			while (isDigit(peek())) {
+				advance();
+			}
+		}
+		if ((peek() == '.' || source.charAt(start) == '.') && isDigit(peekNext()))
+		{
+			advance();
+
+			while (isDigit(peek()))
+			{
+				advance();
+			}
+		}
+
+		addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+	}
+
+	private void identifier()
+	{
+		while (isAlphaNumeric(peek()))
+		{
+			advance();
+		}
+
+		String text = source.substring(start, current);
+		TokenType type = keywords.get(text);
+
+		if (type == null)
+		{
+			type = IDENTIFIER;
+		}
+		addToken(type);
+	}
+
+	private char peekNext()
+	{
+		if (current + 1 >= source.length())
+			return '\0';
+		return source.charAt(current + 1);
+	}
+
+	private void blockComment()
+	{
+		int level = 0;
+
+		do
+		{
+			if(isAtEnd())
+			{
+				Lox.error(line, "Unterminated block comment n'shit");
+				return;
+			}
+			else if (peek() == '\n')
+			{
+				line++;
+			}
+			else if (peek() == '/' && peekNext() == '*')
+			{
+				level++;
+				advance();
+			}
+			else if (peek() == '*' && peekNext() == '/')
+			{
+				if (level == 0)
+				{
+					break;
+				}
+
+				level--;
+				advance();
+			}
+
+			advance();
+
+		} while (true);
+		advance();
+		advance();
 	}
 }
